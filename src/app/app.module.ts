a4aa4989e877c9bf1aac1eb4834942a1dc7ef3c8
Jandronit <alejandro.arguello@bosonit.com>
2021-06-05 12:59:37 +0200

    Proyecto GifsApp Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
 
 import { AppComponent } from './app.component';
 
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, SharedModule, GifsModule],
+  providers: [SidebarComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
